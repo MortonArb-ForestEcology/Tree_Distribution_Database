5d3f7738bda193b468efd809f070ddafde552cc0
Elizabeth <doctoretdiez@comcast.net>
2018-02-27 14:42:21 -0600

    write function to extract PRISM data
    PRISM can be extracted for any given input species

--- scripts/add_climate_predictors_to_species_df.R
+++ scripts/add_climate_predictors_to_species_df.R
@@ -13,8 +13,22 @@
 #                 Quercus arkansana at localized level: ark2_coord.csv
 #                 Quercus arkansana at edited localized level: ark3_coord.csv
 #
+#               Also: climate RasterLayers from PRISM database (.bil files)
+####### Find PRISM data on ULMUS server in  /home/data/PRISM
+###### # unit: mm
+# 30-year normal annual precipitation in mm
+#"PRISM_ppt_30yr_normal_4kmM2_annual_bil.bil"
+# 
+# 30-year normal temperatures in degrees C
+# mean "PRISM_tmean_30yr_normal_4kmM2_annual_bil.bil"
+# maximum "PRISM_tmax_30yr_normal_4kmM2_annual_bil.bil"
+# minimum "PRISM_tmin_30yr_normal_4kmM2_annual_bil.bil"
 #
-#     package: dplyr
+#####
+##### use raster, rgdal and sp packages to read PRISM data
+#
+#
+#     package: dplyr, raster, rgdal, sp
 #
 #     ############### OUTPUT: above files with extra columns holding climate and
 #                     environmental predictor data
@@ -22,3 +36,54 @@
 #                     ex. ark1_cp.csv
 #                         ark2_cp.csv
 #                         ark3_cp.csv
+
+library(sp)
+library(raster)
+library(rgdal)
+library(dplyr)
+
+# decide which species data you would like to work with
+#ark1 <- 
+#ino1 <- 
+#ark2 <- 
+#ino2 <- 
+#ark3 <- 
+#ino3 <- 
+
+# set wd for PRISM data
+# setwd("C:/Users/Elizabeth/Desktop/2017_CTS_fellowship/PRISM")
+
+# and extract the appropriate climate RasterLayers
+annual_ppt <- raster("PRISM_ppt_30yr_normal_4kmM2_annual_bil.bil")
+annual_mean_temp <- raster("PRISM_tmean_30yr_normal_4kmM2_annual_bil.bil")
+annual_max_temp <- raster("PRISM_tmax_30yr_normal_4kmM2_annual_bil.bil")
+annual_min_temp <- raster("PRISM_tmin_30yr_normal_4kmM2_annual_bil.bil")
+
+
+# in this function, extract all PRISM values that you want
+extract_PRISM <- function(d.f){
+  # make sure order is LON LAT
+    d.f_coord <- d.f[, c("long", "lat")]  
+    # extract precipitation data from PRISM
+    d.f_ppt <- extract(annual_ppt, d.f_coord)
+    # extract mean temperature data
+    d.f_met <- extract(annual_mean_temp, d.f_coord)
+    # extract max temp
+    d.f_mat <- extract(annual_max_temp, d.f_coord)
+    # extract min temp
+    d.f_mit <- extract(annual_min_temp, d.f_coord)
+
+    # and add all the new values to the end of the data frame    
+    d.f_coord$annual_ppt <- d.f_ppt
+    d.f_coord$mean_annual_temp <- d.f_met
+    d.f_coord$max_annual_temp <- d.f_mat
+    d.f_coord$min_annual_temp <- d.f_mit
+    # now this is what you will save moving forward, just the numbers
+    print(d.f_coord)
+}
+
+ark2_prism <- extract_PRISM(ark2)
+
+# Now write a csv for future use.
+write.csv(ark2_prism, file = "ark2_prism.csv")
+
